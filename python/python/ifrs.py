8e0e8d33b9716f6779f05cd6024ccd5edf333c46
stefaniaszz <131990404+stefaniaszz@users.noreply.github.com>
2025-04-04 20:37:04 -0300

    aula

--- python/python/ifrs.py
+++ python/python/ifrs.py
@@ -1,6 +1,7 @@
-nota = input()
-nota = float(nota)
-if nota >= 7.0:
-    print('Passou')
-else:
-    print('Não deu')
+for i in range(5): #faça 5 vezes 
+    nota = input()
+    nota = float(nota)
+    if nota >= 7.0:
+        print('Passou')
+    else:
+        print('Não deu')
