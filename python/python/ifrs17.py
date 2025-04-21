f55415e8d1630c564c7f9e34966900a49e4e88e2
stefaniaszz <131990404+stefaniaszz@users.noreply.github.com>
2025-04-20 21:47:05 -0300

    auala

--- python/python/ifrs17.py
+++ python/python/ifrs17.py
@@ -1,9 +1,9 @@
 n = int(input())
-for _ in range(n):
-    k = int(input())
-    total_tomadas = 1
-    for _ in range(k):
-        tomadas = int(input())
-        total_tomadas += tomadas
-total_aparelhos = total_tomadas - k + 1
-print('%d' % total_aparelhos)
+for i in range(n):
+    filtros = [int(x) for x in input().split()]
+    if not filtros:
+        print()
+        continue
+    dif = filtros[0] - 1
+    qtd_filtro = sum(filtros[1:])
+    print(qtd_filtro - dif)
