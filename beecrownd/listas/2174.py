cf192aba5d78acffa654fdb411e3e9bef70abe14
stefaniaszz <131990404+stefaniaszz@users.noreply.github.com>
2025-06-21 12:39:57 -0300

    beecrownd

---
+++ beecrownd/listas/2174.py
@@ -0,0 +1,9 @@
+mochila = []
+n = int(input())
+for i in range(n):
+    pomekon = input()
+    if pomekon != mochila:
+        mochila.append(pomekon) 
+print('Falta(m) %d pomekon(s).')
+
+             
