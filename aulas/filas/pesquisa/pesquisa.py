ab426d97458d38d5ced888e112bfb80e450c8e62
stefaniaszz <131990404+stefaniaszz@users.noreply.github.com>
2025-08-29 22:02:30 -0300

    pesquisa

---
+++ aulas/filas/pesquisa/pesquisa.py
@@ -0,0 +1,16 @@
+def pesquisa_linear (lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i+1
+    return 0
+
+arq = open("palavrasDesord.txt", "r") #abre o arquivo importado e readlines
+palavras = arq.readlines()
+for i in range(len(palavras)):
+    palavras[i] = palavras[i].rstrip() #tira o \n do enter no final das palavras ou espaços desnecessários
+arq.close()
+while True:
+    palavra = input("palavra a pesquisar: ")
+    pos = pesquisa_linear(palavras, palavra)
+    print(pos)
+
